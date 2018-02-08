292524efbaaa53a63b7aa7bb6b89e8c148d59a1a
Pascal Simon <simon@puzzle.ch>
2018-02-08 09:22:28 +0100

    add reseed job to demo seeds

---
+++ db/seeds/demo/delayed_jobs.rb
@@ -0,0 +1,7 @@
+#  Copyright (c) 2006-2018, Puzzle ITC GmbH. This file is part of
+#  PuzzleTime and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/puzzle/puzzletime.
+
+
+ReseedJob.new.schedule
