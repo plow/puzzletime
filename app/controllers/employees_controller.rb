23a1a8e1804d51c38fdc223812ca0248b7ed3df7
Daniel Illi <illi@puzzle.ch>
2019-01-09 17:40:48 +0100

    Initial implementation of jsonapi, refs #46483

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -5,8 +5,10 @@
 
 
 class EmployeesController < ManageController
+  include Scopable
+  include DryCrudJsonapi
 
-  self.permitted_attrs = [:firstname, :lastname, :shortname, :email, :ldapname,
+  self.permitted_attrs += [:firstname, :lastname, :shortname, :email, :ldapname,
                           :department_id, :crm_key, :probation_period_end_date,
                           :graduation, :management, :phone_office, :phone_private,
                           :street, :postal_code, :city, :birthday, :emergency_contact_name,
@@ -23,13 +25,18 @@ class EmployeesController < ManageController
   self.sort_mappings = { department_id: 'departments.name' }
 
   def show
-    if Crm.instance.present?
-      person = Crm.instance.find_people_by_email(entry.email).first
-      if person
-        redirect_to Crm.instance.contact_url(person.id)
-      else
-        flash[:alert] = "Person mit Email '#{entry.email}' nicht gefunden in CRM."
+    super do |format|
+      format.html do
+        if Crm.instance.present?
+          person = Crm.instance.find_people_by_email(entry.email).first
+          if person
+            redirect_to Crm.instance.contact_url(person.id)
+          else
+            flash[:alert] = "Person mit Email '#{entry.email}' nicht gefunden in CRM."
+          end
+        end
       end
+      yield(format) if block_given?
     end
   end
 
@@ -70,6 +77,6 @@ class EmployeesController < ManageController
   private
 
   def list_entries
-    super.includes(:department).references(:department)
+    scope_entries_by(super.includes(:department, :current_employment).references(:department), :current)
   end
 end
